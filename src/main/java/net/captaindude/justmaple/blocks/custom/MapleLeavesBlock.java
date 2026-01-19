f735ad918b01ff1843fce68fbfbe7488a3770733
Capta1nDude <ethan.c@outlook.com.au>
2026-01-20 09:48:38 +1100

    Added tree particles

--- src/main/java/net/captaindude/justmaple/blocks/custom/MapleLeavesBlock.java
+++ src/main/java/net/captaindude/justmaple/blocks/custom/MapleLeavesBlock.java
@@ -1,13 +1,15 @@
 package net.captaindude.justmaple.blocks.custom;
 
+import net.captaindude.justmaple.particle.ModParticles;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
+import net.minecraft.particle.ParticleUtil;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
 public class MapleLeavesBlock extends LeavesBlock {
-
     public MapleLeavesBlock(Settings settings) {
         super(settings);
     }
@@ -17,13 +19,13 @@ public class MapleLeavesBlock extends LeavesBlock {
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) { 
         // Carries over any vanilla leaf logic e.g. rain drops
         super.randomDisplayTick(state, world, pos, random);
-        // if (random.nextInt(10) == 0) {
-        //     BlockPos blockPos = pos.down();
-        //     BlockState blockState = world.getBlockState(blockPos);
-        //     if (!isFaceFullSquare(blockState.getCollisionShape(world, blockPos), Direction.UP)) {
-        //         ParticleUtil.spawnParticle(world, pos, random, ModParticles.MAPLE_LEAVES);
-        //     }
-        // }
+        if (random.nextInt(10) == 0) {
+            BlockPos blockPos = pos.down();
+            BlockState blockState = world.getBlockState(blockPos);
+            if (!isFaceFullSquare(blockState.getCollisionShape(world, blockPos), Direction.UP)) {
+                ParticleUtil.spawnParticle(world, pos, random, ModParticles.MAPLE_LEAVES);
+            }
+        }
     }
 
 }
