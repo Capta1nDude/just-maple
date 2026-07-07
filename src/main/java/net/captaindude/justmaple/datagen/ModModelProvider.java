f64238f03da407737c34c6a856c85873224df9dd
Capta1nDude <ethan.c@outlook.com.au>
2026-07-07 11:24:45 +1000

    Update to 1.21.4

--- src/main/java/net/captaindude/justmaple/datagen/ModModelProvider.java
+++ src/main/java/net/captaindude/justmaple/datagen/ModModelProvider.java
@@ -1,14 +1,13 @@
 package net.captaindude.justmaple.datagen;
 
 import net.captaindude.justmaple.blocks.ModBlocks;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.TexturedModel;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.TexturedModel;
 
 public class ModModelProvider extends FabricModelProvider{
-
     public ModModelProvider(FabricDataOutput output) {
         super(output);
     }
@@ -19,7 +18,7 @@ public class ModModelProvider extends FabricModelProvider{
 
         blockStateModelGenerator.registerLog(ModBlocks.MAPLE_LOG).log(ModBlocks.MAPLE_LOG).wood(ModBlocks.MAPLE_WOOD);
         blockStateModelGenerator.registerLog(ModBlocks.STRIPPED_MAPLE_LOG).log(ModBlocks.STRIPPED_MAPLE_LOG).wood(ModBlocks.STRIPPED_MAPLE_WOOD);
-        blockStateModelGenerator.registerTintableCross(ModBlocks.MAPLE_SAPLING, BlockStateModelGenerator.TintType.NOT_TINTED);
+        blockStateModelGenerator.registerTintableCross(ModBlocks.MAPLE_SAPLING, BlockStateModelGenerator.CrossType.NOT_TINTED);
 
         blockStateModelGenerator.registerSingleton(ModBlocks.MAPLE_LEAVES, TexturedModel.LEAVES);
 
--- src/main/java/net/captaindude/justmaple/particle/custom/MapleLeavesParticle.java
+++ src/main/java/net/captaindude/justmaple/particle/custom/MapleLeavesParticle.java
@@ -1,15 +1,15 @@
 package net.captaindude.justmaple.particle.custom;
 
-import net.minecraft.client.particle.CherryLeavesParticle;
+import net.minecraft.client.particle.LeavesParticle;
 import net.minecraft.client.particle.SpriteProvider;
 import net.minecraft.client.world.ClientWorld;
 
-public class MapleLeavesParticle extends CherryLeavesParticle {
+public class MapleLeavesParticle extends LeavesParticle {
     public MapleLeavesParticle(
             ClientWorld world,
             double x, double y, double z,
             SpriteProvider spriteProvider
     ) {
-        super(world, x, y, z, spriteProvider);
+        super(world, x, y, z, spriteProvider, 0.25F, 2.0F, false, true, 1.0F, 0.0F);
     }
 }
